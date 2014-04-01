3b739f5b7e944a6dcef5ac86a20dad02057fbe51
Derek Keith <indesicant@gmail.com>
2014-04-01 03:05:20 -0600

    Implenting fixed navigation on scroll.

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -1,32 +1,60 @@
 /* NAVIGATION FUNCTIONS */
+
 $(function() {
 	applyClickEvent();
-	function applyClickEvent() {
-		$('a[href*=#]').click(function(e)
-					{
-						e.preventDefault();
-						if( $( $.attr(this, 'href') ).length > 0 ) {
-							$('html, body').animate({
-								scrollTop: $( $.attr(this, 'href') ).offset().top
-							}, "slow");
-						}
-						else {
-							$('html, body').animate({
-								scrollTop: 0
-							}, "slow");
-						}
-						return false;
+	applyScrollSpy();
+	applyStickyNavigation();
+});
+
+$(window).scroll(function() {
+	if(($(window).scrollTop()) > 80){
+		$('#gototop').css({
+			opacity : 1
+		});
+	} else {
+		$('#gototop').css({
+			opacity : 0
 		});
 	}
-	$(window).scroll(function() {
-		if(($(window).scrollTop()) > 80){
-			$('#gototop').css({
-				opacity : 1
-			});
-		} else {
-			$('#gototop').css({
-				opacity : 0
-			});
-		}
-	});
 });
+
+function applyClickEvent() {
+	$('a[href*=#]').click(function(e)	{
+		e.preventDefault();
+		if( $( $.attr(this, 'href') ).length > 0 ) {
+			$('html, body').animate({
+				scrollTop: $( $.attr(this, 'href') ).offset().top
+			}, "slow");
+		}	else {
+			$('html, body').animate({
+				scrollTop: 0
+			}, "slow");
+		}
+		return false;
+	});
+}
+
+function applyScrollSpy() {
+	$('#nav').on('activate.bs.scrollspy', function() {
+		window.location.hash = $('.nav .active a').attr('href').replace('#', '#/');
+	});
+}
+
+function applyStickyNavigation() {
+	lnStickyNavigation = $('#hero').offset().top + 20;	
+	$(window).on('scroll', function() {  
+		stickyNavigation();  
+	});  
+	stickyNavigation();
+}
+
+function stickyNavigation()
+{         
+	if($(window).scrollTop() > lnStickyNavigation) {   
+		$('#nav').addClass('navbar-fixed-top');  
+	} else {  
+		$('#nav').removeClass('navbar-fixed-top');   
+	}  
+}
+
+
