497eb1f94ce66bb92dc4254f9bc6431502b9eded
Derek Keith <indesicant@gmail.com>
2014-04-20 15:29:17 -0600

    Pushing changes to postgresql DB.

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,11 @@ module Codeurge
     # config.i18n.default_locale = :de
 		# add app/assets/fonts to the asset path
 		config.assets.paths << Rails.root.join("app", "assets", "fonts")
+		config.before_configuration do
+				env_file = File.join(Rails.root, 'config', 'local_env.yml')
+				YAML.load(File.open(env_file)).each do |key, value|
+				ENV[key.to_s] = value
+			end if File.exists?(env_file)
+		end
   end
 end
