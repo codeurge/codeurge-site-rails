721c562097c72fabc4002694949491abf84676cf
Derek Keith <derek@codeurge.com>
2014-05-25 12:07:52 -0600

    Add two blog posts to the home page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,7 @@
 class PagesController < ApplicationController
+
+
 	def home
-		@articles = Article.includes(:comments).order("created_at asc").limit(2)
-	end
-
-	def about
-	end
-
-	def services
-	end
-	
-	def contact
+		@articles = Article.all.includes(:comments).order("created_at asc").limit(2)
 	end
 end
