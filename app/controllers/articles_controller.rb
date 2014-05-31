386d196108803a9b8cc769cf90de8d8041d9703c
Derek Keith <derek@codeurge.com>
2014-05-31 05:58:42 -0600

    Add recaptcha to articles comments, home now shows blog post image and post metadata.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,16 @@
 class ArticlesController < ApplicationController
+
+  before_filter :require_login, except: [:index, :show]
+  before_filter :all_categories
+
 	include ArticlesHelper
 
 	def index
-		@articles = Article.all.limit(10).reorder('created_at DESC')
+    if current_user
+     @articles = Article.all.limit(10).reorder('created_at DESC')
+    else
+      @articles = Article.where('articles.published = TRUE').limit(10).reorder('created_at DESC')
+    end
 	end
 
 	def show
@@ -18,11 +26,14 @@ class ArticlesController < ApplicationController
 	def create
 		@article = Article.new(article_params) # Pulls the article hash and assigns it to one with the same keys
 		@article.author_id = current_user.id
-		@article.save
-
-		flash.notice = "Article #{@article.title} Created!"
-
-		redirect_to article_path(@article)
+    if verify_recaptcha
+      @article.save
+      flash.notice = "Article #{@article.title} Created!"
+      redirect_to article_path(@article)
+    else
+      flash.error = "Article #{@article.title} not created!"
+      render :new
+    end
 	end
 
 	def destroy
@@ -48,8 +59,6 @@ class ArticlesController < ApplicationController
 	end
 
 	private
-		before_filter :require_login, except: [:index, :show]
-		before_filter :all_categories
 		def all_categories
 			@categories = Category.all
 		end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,5 +1,5 @@
 module ArticlesHelper
 	def article_params
-		params.require(:article).permit(:title, :body, :category_list, :tag_list, :image)
+		params.require(:article).permit(:title, :body, :category_list, :tag_list, :image, :published)
 	end
 end
